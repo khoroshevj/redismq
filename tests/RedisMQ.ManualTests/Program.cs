1f8d6d5e10970555b8ad6a68f7b143d265f6fbfd
khoroshevj <evgeniy.khoroshev@inn.ru>
2018-02-10 14:35:03 +0300

    sln structure

--- tests/RedisMQ.ManualTests/Program.cs
+++ tests/RedisMQ.ManualTests/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using StackExchange.Redis;
 
-namespace RedisMQ
+namespace RedisMQ.ManualTests
 {
     public class Program
     {
